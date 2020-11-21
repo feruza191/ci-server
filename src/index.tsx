c96cab6c51000a72e228904ad1b0ca1ec92a782e
Feruza Beknazarova <feruza@Feruzas-MacBook-Pro.local>
2020-11-21 23:32:08 +0100

    lint staged fix tsc

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import Img from './images/m3.jpg';
 
 const App: FC = () => (
 	<Wrapper>
-		<p>Hello world!!cdcdscdsbhjbvhj</p>
+		<p>Hello world!!cdcdscdvhj</p>
 		<img src={Img} />
 	</Wrapper>
 );
