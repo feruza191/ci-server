c043b8c51c35463c456782d2a258255a06d9f3b1
Feruza Beknazarova <feruza@Feruzas-MacBook-Pro.local>
2021-01-10 23:34:28 +0100

    fix port tupeorm cinfig

--- server/typeormconfig.ts
+++ server/typeormconfig.ts
@@ -5,7 +5,7 @@ import { Job } from './src/Jobs/entity/job';
 export const typeOrmConfig: ConnectionOptions = {
 	type: 'postgres',
 	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
+	port: Number(process.env.DB_PORT),
 	username: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
